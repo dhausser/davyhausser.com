3503b289607bda9c63de413dc73a8bc2cef3f483
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2020-01-19 22:27:42 +0100

    pwa manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,20 @@ module.exports = {
     `gatsby-plugin-emotion`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `DavyHausserPortfolio`,
+        short_name: `DavyHausserPortfolio`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
   ],
