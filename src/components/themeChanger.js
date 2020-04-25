20eb9d01df229f6d6c6de3902f7c60c31c1507dc
Davy Hausser <davy.hausser@gmail.com>
2020-04-25 14:25:03 +0200

    themechanger styles

--- src/components/themeChanger.js
+++ src/components/themeChanger.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
-import { ThemeChanger } from "./styles"
+import { ThemeChanger, ModeContainer, Sun, Moon } from "./styles"
 
 export default () => (
   <ThemeToggler>
@@ -11,10 +11,10 @@ export default () => (
           onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
           checked={theme === 'dark'}
         />{' '}
-        <div className="mode-container">
-          <i className="gg-sun"></i>
-          <i className="gg-moon"></i>
-        </div>
+        <ModeContainer>
+          <Sun />
+          <Moon />
+        </ModeContainer>
       </label>
     )}
   </ThemeToggler>
