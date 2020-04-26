b2df96030fc36eeb3423a6be6f472ad9edc62f09
Davy Hausser <davy.hausser@gmail.com>
2020-04-26 23:55:46 +0200

    contact form tests

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,12 +12,6 @@ export default function Footer() {
           margin: 40px;
         `}
       >
-        <div>
-          <form name="footer-form" method="post" data-netlify="true">
-            <input type="text" name="message" placeholder="message" />
-            <button type="submit">Send</button>
-          </form>
-        </div>
         <p>
           &copy; 2020 Davy Hausser &bull; Crafted with{" "}
           <span role="img" aria-label="bolt">
