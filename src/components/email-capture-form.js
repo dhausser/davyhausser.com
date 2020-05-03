df552d2130afa584d19f6c86ba91216c115638b7
Davy Hausser <davy.hausser@gmail.com>
2020-05-03 15:12:00 +0200

    email-capture-from integration from gatsbyjs.org

--- src/components/email-capture-form.js
+++ src/components/email-capture-form.js
@@ -9,31 +9,51 @@ import { mediaQueries } from "gatsby-design-tokens/dist/theme-gatsbyjs-org"
 import { themedInput, formInputFocus, buttonStyles } from "../utils/styles"
 
 export default () => (
-  <form method="post" name="email-newsletter-form" data-netlify="true">
-    <input
-      id="email"
-      name="email"
-      type="email"
-      required
-      autoComplete="email"
-      aria-label="Email"
-      placeholder="your.email@example.com"
-      sx={{
-        ...themedInput,
-        width: `100%`,
-        "&:focus": {
-          ...formInputFocus
-        }
-      }}
-    />
-    <button
-      type="submit"
-      sx={{
-        ...buttonStyles().default,
-        mt: 3
-      }}
-    >
-      Subscribe
-    </button>
+  <form
+    method="post"
+    action="https://getform.io/f/8336d95c-917d-402d-8f62-02e80b686cf4"
+    method="POST"
+  >
+    <p>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        autoComplete="name"
+        aria-label="name"
+      />
+    </p>
+    <p>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        required
+        autoComplete="email"
+        aria-label="Email"
+        placeholder="Email"
+        sx={{
+          ...themedInput,
+          width: `100%`,
+          "&:focus": {
+            ...formInputFocus
+          }
+        }}
+      />
+    </p>
+    <p>
+      <textarea name="message" id="" cols="80" rows="5"></textarea>
+    </p>
+    <p>
+      <button
+        type="submit"
+        sx={{
+          ...buttonStyles().default,
+          mt: 3
+        }}
+      >
+        Send
+      </button>
+    </p>
   </form>
 )
