53bd98bef6cd210159fbf6b123172c518bf0aef3
Davy Hausser <davy.hausser@gmail.com>
2020-09-06 17:25:16 +0200

    :sparkles: @sendgrid/mail integration with contact form submission complete

--- functions/contact.js
+++ functions/contact.js
@@ -1,10 +1,30 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 exports.handler = async (event) => {
   const { email, message } = JSON.parse(event.body)
+  const msg = {
+    to: 'davy.hausser@gmail.com',
+    from: 'davy.hausser@icloud.com',
+    subject: 'New contact lead from davyhausser.com',
+    html: `<strong>email:</strong> ${email} <strong>message:</strong> ${message}`,
+  }
 
-  console.log({ email, message })
+  try {
+    await sgMail.send(msg)
+    return {
+      statusCode: 200,
+    }
+  } catch (error) {
+    console.error(error)
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ email, message }),
+    if (error.response) {
+      console.error(error.response.body)
+      return {
+        statusCode: 500,
+        body: error.response.body.message,
+      }
+    }
   }
 }
