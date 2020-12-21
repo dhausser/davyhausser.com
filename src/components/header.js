3a0d3171939b8f539b3f359369a00a17b4e8eba8
Davy Hausser <davy.hausser@gmail.com>
2020-12-21 22:19:58 +0100

    Helmet added to Layout

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,16 @@ import DarkModeToggle from "./dark-mode-toggle"
 import theme from "../../config/theme"
 
 const SocialNavItem = ({ href, title, children }) => (
-  <a href={href} target="_blank" rel="noopener noreferrer" title={title}>
+  <a
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    title={title}
+    css={css`
+      padding: 10 10 10 10;
+      margin: 3rem auto;
+    `}
+  >
     {children}
   </a>
 )
@@ -21,29 +30,48 @@ function Header({ siteTitle, isDark, setIsDark }) {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+        align-items: stretch;
         margin: 3rem 3rem;
       `}
     >
       <nav>
         <span>{siteTitle}</span>
       </nav>
-      <div>
-        <SocialNavItem href="https://github.com/dhausser" title="GitHub">
-          <GoMarkGithub />
-        </SocialNavItem>
-        <SocialNavItem
-          href="https://www.twitter.com/davyhausser"
-          title="Twitter"
+      <div
+        css={css`
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        `}
+      >
+        <div
+          css={css`
+            height: 0px;
+          `}
         >
-          <FaTwitter />
-        </SocialNavItem>
-        <SocialNavItem
-          href="https://www.linkedin.com/in/davyhausser"
-          title="Linkedin"
-        >
-          <FaLinkedin />
-        </SocialNavItem>
-        <DarkModeToggle isDark={isDark} setIsDark={setIsDark} />
+          <SocialNavItem href="https://github.com/dhausser" title="GitHub">
+            <GoMarkGithub
+              css={css`
+                height: 50px;
+              `}
+            />
+          </SocialNavItem>
+          <SocialNavItem
+            href="https://www.twitter.com/davyhausser"
+            title="Twitter"
+          >
+            <FaTwitter />
+          </SocialNavItem>
+          <SocialNavItem
+            href="https://www.linkedin.com/in/davyhausser"
+            title="Linkedin"
+          >
+            <FaLinkedin />
+          </SocialNavItem>
+        </div>
+        <div>
+          <DarkModeToggle isDark={isDark} setIsDark={setIsDark} />
+        </div>
       </div>
     </header>
   )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,26 @@
 /** @jsx jsx */
 import { useState } from "react"
 import { Global, jsx, css, keyframes, ThemeProvider } from "@emotion/react"
+import { Helmet } from "react-helmet"
+import { graphql, useStaticQuery } from "gatsby"
 import Header from "./header"
 import { breakpoints } from "../utils/styles"
 import { themeDark, themeLight } from "../../config/theme"
 
-function globalStyles(theme) {
-  return css`
-    html,
-    body,
-    h1 {
-      background-color: ${theme.background};
-      color: ${theme.text};
-    }
-    a {
-      text-decoration: none;
-    }
-  `
-}
+const globalStyles = (theme) => css`
+  html,
+  body,
+  h1,
+  svg {
+    background-color: ${theme.background};
+    color: ${theme.text};
+  }
+  a {
+    text-decoration: none;
+  }
+`
 
-const FadeOut = keyframes`
+const fadeIn = keyframes`
   0%{
     opacity: 0;
   }
@@ -30,34 +31,60 @@ const FadeOut = keyframes`
 `
 
 export default function Layout({ siteTitle = "Davy Hausser", children }) {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+        }
+      }
+    }
+  `)
+  const {
+    site: {
+      siteMetaData: { title, description, author },
+    },
+  } = data
   const [isDark, setIsDark] = useState(false)
   const theme = isDark ? themeDark : themeLight
   return (
     <ThemeProvider theme={isDark ? themeDark : themeLight}>
       <Global styles={globalStyles(theme)} />
-      <Header siteTitle={siteTitle} isDark={isDark} setIsDark={setIsDark} />
-      <div
-        css={(theme) => css`
-          min-height: 100vh;
-          padding: 20px;
-          margin: 25px auto;
-          opacity: 0;
-          animation: ${FadeOut} 0.6s 0.3s ease-in-out forwards;
-
-          @media (min-width: ${breakpoints.tabletMin}) {
-            max-width: 940px;
-            padding: 20px;
-            margin: 75px auto;
-          }
-
-          @media (min-width: ${breakpoints.desktopMin}) {
-            max-width: 1200px;
-            padding: 20px;
-            margin: 75px auto;
-          }
-        `}
+      <Helmet
+        title={title}
+        meta={[{ name: "description", content: description }]}
       >
-        {children}
+        <html lang="en" />
+        <script src="https://js.tito.io/v1" async />
+        <noscript>This site runs best with JavaScript enabled.</noscript>
+      </Helmet>
+      <div>
+        <Header siteTitle={siteTitle} isDark={isDark} setIsDark={setIsDark} />
+        <div
+          css={css`
+            min-height: 100vh;
+            padding: 20px;
+            margin: 25px auto;
+            opacity: 0;
+            animation: ${fadeIn} 0.6s 0.3s ease-in-out forwards;
+
+            @media (min-width: ${breakpoints.tabletMin}) {
+              max-width: 940px;
+              padding: 20px;
+              margin: 75px auto;
+            }
+
+            @media (min-width: ${breakpoints.desktopMin}) {
+              max-width: 1200px;
+              padding: 20px;
+              margin: 75px auto;
+            }
+          `}
+        >
+          {children}
+        </div>
       </div>
     </ThemeProvider>
   )
