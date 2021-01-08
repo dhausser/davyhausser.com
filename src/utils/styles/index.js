c31b3825838b55d41d3862f240a672ee89950120
Davy Hausser <davy.hausser@gmail.com>
2021-01-08 16:27:42 +0100

    :fire: deleted unused styles

--- src/utils/styles/index.js
+++ src/utils/styles/index.js
@@ -1,120 +1,7 @@
 import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 
-import {
-  colors,
-  space,
-  transition,
-  radii,
-  fontSizes,
-  fontWeights,
-  fonts,
-} from 'gatsby-design-tokens/dist/theme-gatsbyjs-org'
-
 export { default as globalStyles } from './global'
-
-const stripeAnimation = keyframes({
-  '0%': { backgroundPosition: `0 0` },
-  '100%': { backgroundPosition: `${space[7]} ${space[11]}` },
-})
-
-export const focusStyle = {
-  outline: 0,
-  boxShadow: `0 0 0 2px ${colors.input.focusBoxShadow}`,
-}
-
-export const StyledButton = styled.button`
-  align-items: center;
-  background-color: ${props => props.theme.primaryBg};
-  border-radius: 2;
-  border-width: 1;
-  border-style: solid;
-  border-color: ${props => props.theme.primaryBorder};
-  color: ${props => props.theme.primaryText};
-  cursor: pointer;
-  display: inline-flex;
-  font-family: heading;
-  font-weight: bold;
-  flex-shrink: 0;
-  line-height: solid;
-  text-decoration: none;
-  white-space: nowrap;
-  padding: 3 0;
-  height: 36px;
-  background-size: ${props =>
-    `${props.theme.space[7]} ${props.theme.space[7]}`};
-  transition: ${props => `all ${props.theme.transition.default}`};
-  :hover, :focus {
-    background-color: ${props => props.theme.gatsby};
-    background-image: linear-gradient(135deg, rgba(0,0,0, 0.1) 25%, transparent 25%, transparent 50%, rgba(0,0,0, 0.1) 50%, rgba(0,0,0, 0.1) 75%, transparent 75%, transparent);
-    color: ${colors.white};
-    animation: ${stripeAnimation} 2.8s linear infinite;
-    border-color: ${props => props.theme.gatsby};
-  };
-  :focus { ${focusStyle} };
-  :after { content: '', display: block };
-  & svg { margin-left: .2em };
-`
-
-// form elements
-export const formInputFocus = {
-  borderColor: colors.input.focusBorder,
-  ...focusStyle,
-}
-
-export const formInput = {
-  backgroundColor: colors.white,
-  border: `1px solid ${colors.input.border}`,
-  borderRadius: `${radii[2]}`,
-  display: `block`,
-  fontFamily: fonts.system,
-  fontSize: fontSizes[2],
-  fontWeight: fontWeights.body,
-  lineHeight: `2.25rem`,
-  py: 0,
-  px: space[2],
-  transition: `box-shadow ${transition.speed.default} ${transition.curve.default}`,
-  verticalAlign: `middle`,
-  width: `100%`,
-  '::placeholder': {
-    color: colors.input.placeholder,
-    opacity: 1,
-  },
-  '&:focus': {
-    ...formInputFocus,
-  },
-  '&:disabled': {
-    cursor: `not-allowed`,
-    opacity: `0.5`,
-  },
-}
-
-export const themedInputFocus = {
-  bg: `themedInput.backgroundFocus`,
-  boxShadow: t => `0 0 0 2px ${t.colors.themedInput.focusBoxShadow}`,
-  outline: 0,
-  width: `100%`,
-}
-
-export const themedInput = {
-  ...formInput,
-  appearance: `none`,
-  bg: `themedInput.background`,
-  border: 0,
-  color: `text`,
-  overflow: `hidden`,
-  px: 3,
-  ':focus': {
-    ...themedInputFocus,
-  },
-  '::placeholder': {
-    color: `themedInput.placeholder`,
-  },
-}
-
-// Layout
-export const blogWidth = `42rem`
-export const breakpointGutter = `@media (min-width: ${blogWidth})`
 
 export const breakpoints = {
   mobileMin: '480px',
@@ -122,13 +9,6 @@ export const breakpoints = {
   tabletMin: '960px',
   tabletMax: '1199px',
   desktopMin: '1200px',
-}
-
-export const pullIntoGutter = {
-  marginLeft: `-${space[6]}`,
-  marginRight: `-${space[6]}`,
-  paddingLeft: space[6],
-  paddingRight: space[6],
 }
 
 export const Float = keyframes`
@@ -143,10 +23,25 @@ export const Float = keyframes`
   }
 `
 
-export const HeadingWrapCont = styled.div`
-  padding-top: 40px;
-  background-position: 0 0;
-  background-repeat: no-repeat;
+export const Heading = styled.h1`
+  width: 100%;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 33px;
+  line-height: 129.69%;
+  letter-spacing: 0.03em;
+  transform: translateY(30px);
+  opacity: 0;
+  animation: ${Float} 1s 0.2s forwards;
+
+  @media (min-width: ${breakpoints.mobileMax}) {
+    font-size: 40px;
+    transform: scale(1);
+  }
+
+  > span {
+    color: #b4e0e8;
+  }
 `
 
 export const HeadingMain = styled.h1`
@@ -185,10 +80,28 @@ export const HeadingMain = styled.h1`
   }
 `
 
+export const HeadingWrapCont = styled.div`
+  padding-top: 40px;
+  background-position: 0 0;
+  background-repeat: no-repeat;
+`
+
 export const Container = styled.div`
   margin: 100px 0 200px 0;
   width: 100%;
   height: auto;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media (min-width: ${breakpoints.mobileMax}) {
+    flex-wrap: nowrap;
+  }
+`
+
+export const SubContainer = styled.div`
+  margin: 0px 0;
+  width: 100%;
   display: flex;
   align-items: center;
   flex-wrap: wrap;
@@ -226,16 +139,8 @@ export const SkillTable = styled.div`
   }
 `
 
-export const SubContainer = styled.div`
-  margin: 0px 0;
+export const SkillContent = styled.div`
   width: 100%;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-
-  @media (min-width: ${breakpoints.mobileMax}) {
-    flex-wrap: nowrap;
-  }
 `
 
 export const TextContent = styled.div`
@@ -260,10 +165,6 @@ export const TextContentWrap = styled(TextContent)`
   }
 `
 
-export const SkillContent = styled.div`
-  width: 100%;
-`
-
 export const Paragraph = styled.p`
   margin: 50px 0;
   width: 100%;
@@ -280,26 +181,5 @@ export const Paragraph = styled.p`
     :hover {
       border-bottom: 2px solid black;
     }
-  }
-`
-
-export const Heading = styled.h1`
-  width: 100%;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 33px;
-  line-height: 129.69%;
-  letter-spacing: 0.03em;
-  transform: translateY(30px);
-  opacity: 0;
-  animation: ${Float} 1s 0.2s forwards;
-
-  @media (min-width: ${breakpoints.mobileMax}) {
-    font-size: 40px;
-    transform: scale(1);
-  }
-
-  > span {
-    color: #b4e0e8;
   }
 `
