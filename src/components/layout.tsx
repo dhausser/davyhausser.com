ceb9a92b18c4c377d3b147bc63d3e90a154d2b25
Davy Hausser <davy.hausser@gmail.com>
2021-03-17 10:54:39 +0100

    migration form emotion to standard css layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,44 +1,17 @@
 import * as React from 'react'
-import { Helmet } from 'react-helmet'
-import { graphql, useStaticQuery } from 'gatsby'
 import { Header } from 'components'
 import 'styles/index.css'
 
 interface Props {
-  children: JSX.Element | JSX.Element[]
+  children: React.ReactElement
 }
 
-function Layout({ children }: Props): JSX.Element {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-        }
-      }
-    }
-  `)
-
-  const {
-    site: {
-      siteMetadata: { title, description },
-    },
-  } = data
-
+function Layout({ children }: Props) {
   return (
-    <>
-      <Helmet
-        title={title}
-        meta={[{ name: 'description', content: description }]}
-      >
-        <html lang="en" />
-        <noscript>This site runs best with JavaScript enabled.</noscript>
-      </Helmet>
-      <Header siteTitle={title} />
+    <div>
+      <Header siteTitle="TITLE" />
       <main>{children}</main>
-    </>
+    </div>
   )
 }
 
